3384cdc60078f05def90cbf285764d8e2fdff03a
zhipzhang <zhipzhang@foxmail.com>
2026-08-17 10:58:17 +0900

    fix: fix fits to h5

--- src/pybkgmodel/data.py
+++ src/pybkgmodel/data.py
@@ -966,7 +966,7 @@ class OffRunSummary:
         file_paths = []
         for obs_id in self.obs_id:
             matching_files = glob.glob(
-                f"{self.path_prefix}/dl2_LST-1.Run{obs_id:05d}.fits", recursive=False
+                f"{self.path_prefix}/dl2_LST-1.Run{obs_id:05d}.h5", recursive=False
             )
             if len(matching_files) == 0:
                 print(f"No file found for obs_id {obs_id}.")
